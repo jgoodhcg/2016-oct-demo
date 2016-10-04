e11ae4b08c4cdc8069594ff953f0177193e0e7fb
jgoodhcg <jgoodhcg@gmail.com>
2016-10-04 08:44:21 -0400

    broken react routes

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-docker-node-frontend
+###todo
+
+ - [ ] favicon
+ - [ ] topbar
+   - [ ] name/username (fade in and out?)
+   - [ ] links
+     - [ ] twitter
+     - [ ] gmail
+     - [ ] linked in
+     - [ ] github
+ - [ ] bio card
+   - [ ] avatar
+   - [ ] bio
+   - [ ] skills button
+   - [ ] collapse
+   
+   
+ - [ ] react indentation
--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,7 @@
 require("./../css/app.css");
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 import Layout from "./components/layout";
 
@@ -10,4 +11,12 @@ app_container.setAttribute("id", "app");
 document.body.appendChild(app_container);
 
 const app = document.getElementById("app");
-ReactDOM.render(<Layout/>, app);
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+      <Route path="/" component={Layout}>
+      <IndexRoute component={Featured}></IndexRoute>
+        <Route path="archives(/:article)" name="archives" component={Archives}></Route>
+        <Route path="settings" name="settings" component={Settings}></Route>
+      </Route>
+    </Router>, app);
