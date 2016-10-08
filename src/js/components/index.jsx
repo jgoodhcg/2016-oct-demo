9b5b568d12c503ae061ab107b42330e53423d469
jgoodhcg <jgoodhcg@gmail.com>
2016-10-08 16:30:28 -0400

    inline svg!

--- src/js/components/index.jsx
+++ src/js/components/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import InlineSVG from 'svg-inline-react';
 
 export default class Index extends React.Component {
     constructor() {
@@ -11,7 +12,7 @@ export default class Index extends React.Component {
                 <div class="container">
                     <div class="row">
                         <div class="col-12">
-                            <img src="./../resources/self-portrait.svg"/>
+                            <InlineSVG src={require("./../../resources/self-portrait.svg")}/>
                         </div>
                     </div>
                 </div>
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -27,8 +27,9 @@ module.exports = {
                 loaders: ["style", "css", "sass"]
             },
             {test: /\.png$/, loader: 'file-loader'},
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader:"url?limit=10000&mimetype=application/font-woff" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file" }
+            {test: /\.svg$/, loader: 'svg-inline?classPrefix'}
+            // { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader:"url?limit=10000&mimetype=application/font-woff" },
+            // { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file" }
         ]
     },
     output: {
