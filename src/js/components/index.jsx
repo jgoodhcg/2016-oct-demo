2640016481e7ff9a708c3b485ff47a64e6e9b006
jgoodhcg <jgoodhcg@gmail.com>
2016-10-08 12:06:53 -0400

    grid guide experiment + resources

--- src/js/components/index.jsx
+++ src/js/components/index.jsx
@@ -8,7 +8,13 @@ export default class Index extends React.Component {
     render() {
         return (
             <div id="title" class="card card-1">
-                stuff here
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <img src="./../resources/self-portrait.svg"/>
+                        </div>
+                    </div>
+                </div>
             </div>
                 );
 
