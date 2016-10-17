f534dc307146f197e515d15992707a0be4f395a9
jgoodhcg <jgoodhcg@gmail.com>
2016-10-16 21:04:32 -0400

    clicky works

--- src/js/components/clicky-comp.jsx
+++ src/js/components/clicky-comp.jsx
@@ -8,9 +8,45 @@ export default class Index extends React.Component {
     constructor() {
         super();
 
+        this.state = {bannerModule : null};
+
 }
     componentDidMount(){
-        window.bannerModule();
+        this.setState(
+            Object.assign(
+                this.state,
+                {bannerModule: window.bannerModule()}));
+
+        var banner = document.getElementById('banner'),
+            bx, by, bstart, bend;
+        // touch
+        banner.addEventListener('touchstart', (e) => {
+            bx = e.touches[0].clientX;
+            by = e.touches[0].clientY;
+            bstart = e.timeStamp;
+        });
+        banner.addEventListener('touchend', (e) => {
+            bend = e.timeStamp;
+            this.ripple(bx,by,bend - bstart);
+        });
+        // mouse
+        banner.addEventListener('mousedown', (e) => {
+            bx = e.clientX;
+            by = e.clientY;
+            bstart = e.timeStamp;
+        });
+        banner.addEventListener('mouseup', (e) => {
+            bend = e.timeStamp;
+            this.ripple(bx,by,bend - bstart);
+        });
+
+        window.onresize = (() => {
+            this.state.bannerModule.reDraw();
+        });
+    }
+
+    ripple(x,y,d){
+        this.state.bannerModule.addRipple(x,y,d);
     }
 
     render() {
